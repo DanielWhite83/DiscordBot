cfa28b8d12d1b709c5d9e3b59d701f0a73bcca65
[Daniel White] <Dan.White.Ku@gmail.com>
2021-04-23 17:52:32 +0900

    Basic Messaging Functionality Added

--- index.js
+++ index.js
@@ -11,6 +11,22 @@ client.once('ready', () => {
     console.log('Bot is online')
 });
 
+client.on('message', message =>{
+    if (!message.content.startsWith(prefix) || message.author.bot) return;
+
+    const args = message.content.slice(prefix.length).split(/ +/);
+    const command = args.shift().toLowerCase();
+
+    if(command == 'ping'){
+        message.channel.send('pong!');
+
+    }
+    else if (command == 'youtube'){
+        message.channel.send('yep!');
+    }
+
+
+});
 
 // must be last line
 client.login(Passwords.login_token);
